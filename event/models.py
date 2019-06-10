e3c5e8110a10f80261580231d1dd52d2e9089ead
rf <rf.fa17@gmail.com>
2019-06-10 15:34:04 +0430

    added price field to event model

--- event/models.py
+++ event/models.py
@@ -9,6 +9,7 @@ class Event(models.Model):
     location = models.CharField(max_length=200, verbose_name='مکان')
     date = models.DateTimeField(verbose_name='زمان')
     duration = models.IntegerField(verbose_name='مدت', default=1)
+    price = models.CharField(max_length=100, default='رایگان', verbose_name='هزینه ثبت نام')
     desc = models.TextField(verbose_name='توضیحات بیشتر', blank=True)
     created = models.DateTimeField(auto_now_add=timezone.now, editable=False, verbose_name='زمان ثبت')
     last_modified = models.DateTimeField(auto_now=timezone.now, verbose_name='آخرین تغییر')
